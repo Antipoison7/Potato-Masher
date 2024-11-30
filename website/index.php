83deb846db493742e1e61cf2449989578dc16cbc
ConnorRMIT <s4096467@student.rmit.edu.au>
2024-11-30 20:55:55 +1100

    Created index

---
+++ website/index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>The Leak World</title>
+
+        <meta charset="UTF-8">
+        <meta name="title" content="The Leak World" />
+        <meta name="keywords" content="Andrew Tate, The Real World, Andrew Tate Hack">
+        <meta name="author" content="Antipoison">
+        <meta name="description" content="Andrew Tate: The Real World JSON message formatter">
+
+        <link rel="stylesheet" href="./resources/css/base.css">
+    </head>
+    <body>
+        
+    </body>
+</html>
